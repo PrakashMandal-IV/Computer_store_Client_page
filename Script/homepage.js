6fc7db9c30e4d500bdbf10c558309019d46461b9
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-14 10:49:27 +0530

    Add : javascript to fetch product from api

--- Script/homepage.js
+++ Script/homepage.js
@@ -1,4 +1,4 @@
-const url = 'https://localhost:7063/Product/get-product-by-id/7'
+const url = 'https://localhost:7063/Product/get-all-product'
 fetch(url,{
     method: 'GET',
     headers:{
@@ -11,8 +11,30 @@ fetch(url,{
 }).then(function (text){
     
        console.log(text)
-        document.getElementById('ProdImage').src = text.imageUrl
-        document.getElementById('producttitle').innerText = text.name
-      
-       
+       featurecard(text)      
 })
+
+function featurecard(data){
+    var cards = document.getElementById('cards')
+    for (var i = 0;i<4;i++)
+    {
+        var card = ` <div class="card">
+            <img src="${data[i].imageUrl}"
+                alt="image" class="productImage" id="ProdImage">
+            <p class="productTitle" id="producttitle">${data[i].name}</p>
+            <div class="category">
+                <p id="category">Laptop</p>
+                <p id="category">Laptop</p>
+            </div>
+            <div class="buy">
+                <button class="buybtn">Buy Now</button>
+                <button class="addcart"><img class="cartImage" src="images/cart.png" alt=""></button>
+                <div class="price">
+                    <p class="mainPrice">${data[i].newPrice}</p>
+                    <p class="ogPrice">₹ ${data[i].price}</p>
+                </div>
+            </div>`
+    console.log(data[i])
+    cards.innerHTML +=card
+    }
+}
