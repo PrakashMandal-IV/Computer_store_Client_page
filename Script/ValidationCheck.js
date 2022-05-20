66c0922342de9ab2645ad5872ee6b601118af370
Prakash <prakashmandal.iv@gmail.com>
2022-05-21 02:22:23 +0530

    Add : login/signup page

--- Script/ValidationCheck.js
+++ Script/ValidationCheck.js
@@ -8,6 +8,7 @@ else{
    fetch(validationUrl,{
        method: 'GET',
        headers: {
+        'Authorization': "bearer " + token,
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Methods": 'OPTIONS,POST,GET',
         'Content-Type': 'application/json; charset=UTF-8'
